1e504a4b6c628066714b1828b1de8a9aa21346a5
Alhoor Sultan <alhooralh@gmail.com>
2023-08-26 09:20:56 +0400

    Initial Commit

---
+++ Part1/Part1App.swift
@@ -0,0 +1,17 @@
+//
+//  Part1App.swift
+//  Part1
+//
+//  Created by Alhoor Sultan  on 26/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct Part1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
